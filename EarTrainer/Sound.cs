eaf1117f137dee6bc1ca4e241720194274850548
Fragg1e <woodwardfraser1@gmail.com>
2026-02-16 15:42:24 +0000

    added sounds and audio player

---
+++ EarTrainer/Sound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace EarTrainer
+{
+    public static class Sound
+    {
+        private static WaveOutEvent output = new WaveOutEvent();
+        private static FadeInOutSampleProvider musicFader;
+
+        public static void PlayMusic(string file, bool fadeIn = true)
+        {
+            output.Stop();
+            var reader = new AudioFileReader($@"..\..\..\sound/{file}.wav"); //gets the audio file        
+
+            musicFader = new FadeInOutSampleProvider(reader, fadeIn); //creates the fader
+
+            reader.Volume = 0.7f;
+
+            output.Init(musicFader); //initialises music 
+
+            output.Play(); //starts it 
+            
+        }
+    }
+}
