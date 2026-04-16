0a7aa5887302bd4f3a52ac5ac545c63d49309533
Fraggle420 <woodwardfraser1@gmail.com>
2026-04-16 15:34:59 +0100

    added database fallback creator

--- EarTrainer/App.xaml.cs
+++ EarTrainer/App.xaml.cs
@@ -1,4 +1,5 @@
 using System.Data.Entity;
+using System.Data.SQLite;
 using System.Windows;
 
 namespace EarTrainer
@@ -9,12 +10,29 @@ namespace EarTrainer
         {
             base.OnStartup(e);
 
+            using (var connection = new SQLiteConnection("Data Source=EarTrainer.db;Version=3;"))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand(
+                    @"CREATE TABLE IF NOT EXISTS HighScores (
+                        HighScoreId INTEGER PRIMARY KEY AUTOINCREMENT,
+                        PlayerName TEXT NOT NULL,
+                        Score INTEGER NOT NULL,
+                        Difficulty TEXT NOT NULL,
+                        DatePlayed DATETIME NOT NULL
+                    )", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
             Database.SetInitializer(new CreateDatabaseIfNotExists<EarTrainerContext>());
 
             using (var db = new EarTrainerContext())
             {
                 db.Database.Initialize(false);
             }
-        }       
+        }
     }
 }
